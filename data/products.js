96afe61d65b3c3f20f32fa31e3a5d141c4b46fa2
kelvinquartey <kelvinquartey1@gmail.com>
2025-01-02 12:40:46 +0000

    generating product html with js

---
+++ data/products.js
@@ -0,0 +1,86 @@
+export const products = [
+    {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+    {
+    id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    image: 'assets/images/shoes/luis-felipe-lins-J2-wAQDckus-unsplash.jpg',
+    brand: 'nike',
+    name: 'Runners',
+    price: 78
+    },
+]
---
+++ js/shop.js
@@ -0,0 +1,85 @@
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js'
+
+let productsHTML = '';
+
+products.forEach((product) => {
+    
+    productsHTML += `
+        <article class="pro js-product modalbtn" data-product-id ="${product.id}">
+            <img class="product-image" src="${product.image}" alt="product 1">
+            <div class="des">
+                <span class="product-brand">${product.brand}</span>
+                <h5 class="product-name">${product.name}</h5>
+                <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                </div>
+                <h4 class="product-price">$${product.price}</h4>
+            </div>
+
+            <button class="add-to-cart-button js-add-to-cart"
+            data-product-id="${product.id}">
+                Add to Cart
+            </button>
+        </article>
+    `;
+
+});
+
+document.querySelector('.js-products-container')
+    .innerHTML = productsHTML;
+
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () =>{
+            const productId = button.dataset.productId;
+            addToCart(productId);
+            updateCartQuantity();
+
+        });
+    })
+
+function updateCartQuantity(){
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    })
+
+    document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+}
+
+
+
+
+
+
+
+
+
+// start of media queries
+
+const bar = document.querySelector('#bar');
+const nav = document.querySelector('.header__nav');
+const close = document.querySelector('#close');
+
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+// end of media queries
+
